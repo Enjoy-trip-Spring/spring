d80f5cf9f1c7d4e5013836955821b29fb3855310
songhyeokjun <songhyeokjun@naver.com>
2023-04-26 17:12:32 +0900

    [Update] 초기 설정 변경 23.04.26 1712

--- enjoy-trip/src/main/java/com/ssafy/board/model/Board.java
+++ enjoy-trip/src/main/java/com/ssafy/board/model/Board.java
@@ -1,5 +1,14 @@
 package com.ssafy.board.model;
 
 public class Board {
+	private String title;
 
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
 }
