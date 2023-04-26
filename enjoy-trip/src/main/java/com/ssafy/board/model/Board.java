728833f1987906120ced5c1a1250f613095b32c6
songhyeokjun <songhyeokjun@naver.com>
2023-04-26 18:00:25 +0900

    [Update] Update Dto 23.04.26 1800

--- enjoy-trip/src/main/java/com/ssafy/board/model/Board.java
+++ enjoy-trip/src/main/java/com/ssafy/board/model/Board.java
@@ -1,14 +1,61 @@
 package com.ssafy.board.model;
 
 public class Board {
-	private String title;
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
+	private int boardNo;
+	private String boardTitle;
+	private String boardContent;	
+	private String userId;	
+	private int hit;
+	private String createDate;	
+	private int boardLike;	
+	private int boardCommentCnt;
 	
+	public int getBoardNo() {
+		return boardNo;
+	}
+	public void setBoardNo(int boardNo) {
+		this.boardNo = boardNo;
+	}
+	public String getBoardTitle() {
+		return boardTitle;
+	}
+	public void setBoardTitle(String boardTitle) {
+		this.boardTitle = boardTitle;
+	}
+	public String getBoardContent() {
+		return boardContent;
+	}
+	public void setBoardContent(String boardContent) {
+		this.boardContent = boardContent;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public int getHit() {
+		return hit;
+	}
+	public void setHit(int hit) {
+		this.hit = hit;
+	}
+	public String getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(String createDate) {
+		this.createDate = createDate;
+	}
+	public int getBoardLike() {
+		return boardLike;
+	}
+	public void setBoardLike(int boardLike) {
+		this.boardLike = boardLike;
+	}
+	public int getBoardCommentCnt() {
+		return boardCommentCnt;
+	}
+	public void setBoardCommentCnt(int boardCommentCnt) {
+		this.boardCommentCnt = boardCommentCnt;
+	}	
 }
